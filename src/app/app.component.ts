406de98c44b565b676c9aea66c92050217cc2e23
calebss <caleb_ss@yahoo.com.br>
2017-07-29 10:56:35 -0300

    correection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { jqxBarGaugeComponent } from 'jqwidgets-framework/jqwidgets-ts/angular_jqxbargauge';
-import JSZip from 'jszip';
+import JSZip from 'jszip/dist/jszip';
 
 
 
